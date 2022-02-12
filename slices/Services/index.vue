6e82de6df7f118a143633405870157a1fb958d09
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-12 19:09:38 +0000

    added Layout for desktop

---
+++ slices/Services/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="grid lg:grid-cols-4">
+    <HeaderPrismic
+      :title="slice.primary.title"
+      :description="slice.primary.description"
+    />
+   
+    <Icons :items="slice.items" />
+  </section>
+</template>
+
+<script>
+import HeaderPrismic from "~/components/base/HeaderPrismic.vue";
+
+import Icons from "~/components/base/Icons.vue";
+export default {
+  components: {
+    Icons,
+    HeaderPrismic
+  },
+  name: "Services",
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
