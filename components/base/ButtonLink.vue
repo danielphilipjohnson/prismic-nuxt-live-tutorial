0c4c923010e5057d181536271ad24c28e9b4bb52
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-12 22:58:22 +0000

    split out button link

---
+++ components/base/ButtonLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <prismic-link
+  :class="buttonClasses"
+    class="block p-4 text-white bg-purple-500 rounded max-w-max"
+    :field="buttonLink"
+    >{{ buttonText }}</prismic-link
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+    },
+    buttonLink: {
+      type: Array,
+    },
+    buttonClasses: {
+      type: String,
+    },
+  },
+};
+</script>
