42dfd6e20ac5c55f34e70aed5539433be5a6f02f
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-12 16:36:07 +0000

    Added dependencies

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+import smConfig from "./sm.json"
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'prismic-temp',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  image: {
+    prismic: {
+  },
+  domains: ['images.prismic.io'],
+  alias: {
+    prismic: 'https://images.prismic.io'
+  }
+},
+prismic: {
+
+  preview: true,
+  modern: true
+},
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    '@nuxt/image',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxt/image',["@nuxtjs/prismic", {
+    endpoint: smConfig.apiEndpoint || "",
+  }], ["nuxt-sm"]],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: ["vue-slicezone", "nuxt-sm"]
+  }
+}
