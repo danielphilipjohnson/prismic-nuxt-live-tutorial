3036eb57bfda1485ec875de1bf32703a00261435
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-13 12:29:59 +0000

    Built accredtations slice

---
+++ slices/Accreditations/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="accreditation-block">
+    <header-description-prismic
+      :title="slice.primary.title"
+      titleClasses="text-4xl font-bold leading-9"
+      :description="slice.primary.subHeading"
+      subHeadingClasses="text-2xl font-bold leading-8"
+    />
+    <div class="image-block">
+      <template class="" v-for="(item, i) in slice.items">
+        <prismic-image :field="item.logo" :key="`slice-item-${i}`" />
+      </template>
+    </div>
+  </section>
+</template>
+
+<script>
+import HeaderDescriptionPrismic from "~/components/base/HeaderDescriptionPrismic.vue";
+
+export default {
+  name: "Accreditations",
+  components: {
+    HeaderDescriptionPrismic,
+  },
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.accreditation-block {
+  @apply bg-gray-200 py-20;
+}
+.image-block {
+  @apply grid grid-cols-4 max-w-7xl mx-auto;
+}
+</style>
