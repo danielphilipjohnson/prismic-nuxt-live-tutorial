40707ba12a6001bc2ba01c9c1072c1c0f802d066
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-13 17:21:40 +0000

    Updated slice machine components

--- .slicemachine/assets/slices/MainHero/index.stories.js
+++ .slicemachine/assets/slices/MainHero/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"main_hero","items":[],"primary":{"title":[{"type":"heading1","text":"Incubate integrated communities","spans":[]}],"subHeading":[{"type":"heading2","text":"Evolve mission-critical communities","spans":[]}],"description":[{"type":"paragraph","text":"Id sunt esse culpa culpa sunt proident. Laborum tempor do id laboris ipsum enim Lorem duis exercitation consequat labore ut officia ullamco. Do est aliqua veniam elit non occaecat eu nulla ipsum Lorem qui.","spans":[]}],"cta":"utilize plug-and-play deliverables","ctaLink":{"link_type":"Web","url":"http://google.com"},"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1491975474562-1f4e30bc9468?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"main_hero","items":[],"primary":{"title":[{"type":"heading1","text":"Unleash end-to-end solutions","spans":[]}],"subHeading":[{"type":"heading2","text":"Drive revolutionary models","spans":[]}],"description":[{"type":"paragraph","text":"Dolore id laborum sit ea deserunt. Voluptate ex eiusmod amet non aliqua fugiat quis eu commodo dolore quis aliquip ipsum ipsum sint.","spans":[]}],"cta":"architect end-to-end convergence","ctaLink":{"link_type":"Web","url":"https://prismic.io"},"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1587614295999-6c1c13675117?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
